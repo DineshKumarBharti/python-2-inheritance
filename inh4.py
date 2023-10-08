ffd9b08c958ecbfc6e49710cdfedaeb18e7b7e25
dinesh <dinesh.bharti.be16@gmail.com>
2023-10-07 17:24:12 -0700

    multiple parent single child inhritance

--- inh4.py
+++ inh4.py
@@ -1,25 +1,31 @@
 class A:
     def get(self):
-        self.x=100
         print("this is get class A . ")
-        print("y:",self.x)
-class B(A):
+
+class B:
     def put(self):
         print("this is put class B..")
-        print("x: ",self.x)
-        print("y: ",self.y)
-class C(A):
+    
+    def get(self):
+        print("this is a  get 2nd")
+        
+class C(A,B):
     def display(self):
         print("this is display class C...")
         print("y:", self.y)
+
+        objb=B()
+        objb.get()
+
+
 objc=C()
 objc.y=500
 objc.get()
 objc.display()
 
-objb=B()
-objb.y=5000
-objb.get()
-objb.put()
 
+# his is get class A .
+# this is display class C...
+# y: 500
+# this is a  get 2nd
 
